3454f5b4b3784eb3ff7add724016931f02ef4199
edomingues <edi.domingues@hurb.com>
2023-06-20 14:23:01 -0300

    :art: changed activity

--- app/src/main/java/com/example/githubusers/users/activity/GitHubUsersActivity.kt
+++ app/src/main/java/com/example/githubusers/users/activity/GitHubUsersActivity.kt
@@ -2,11 +2,24 @@ package com.example.githubusers.users.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.githubusers.R
+import com.example.githubusers.databinding.ActivityGitHubUsersBinding
+import com.example.githubusers.extension.viewBinding
+import com.example.githubusers.users.fragment.UsersFragment
 
 class GitHubUsersActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityGitHubUsersBinding::inflate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_git_hub_users)
+        setContentView(binding.root)
+
+        initFragment()
+    }
+
+    private fun initFragment() {
+        val usersFragment = UsersFragment.newInstance()
+        supportFragmentManager.beginTransaction()
+            .add(binding.clUsers.id, usersFragment)
+            .commit()
     }
 }
