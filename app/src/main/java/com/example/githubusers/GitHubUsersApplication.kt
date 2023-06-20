992e70aad7108f92f88068a4a1aa36087ebedf21
edomingues <edi.domingues@hurb.com>
2023-06-20 14:03:54 -0300

    :art: added initialization to di modules

--- app/src/main/java/com/example/githubusers/GitHubUsersApplication.kt
+++ app/src/main/java/com/example/githubusers/GitHubUsersApplication.kt
@@ -1,11 +1,22 @@
 package com.example.githubusers
 
 import android.app.Application
+import com.example.githubusers.di.networkModule
+import com.example.githubusers.users.usersModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class GitHubUsersApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
-//        initModules()
+        initModules()
+    }
+
+    private fun initModules(){
+        startKoin {
+            androidContext(this@GitHubUsersApplication)
+            modules(networkModule, usersModule)
+        }
     }
 }
