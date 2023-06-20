995384ba7b37a445933841b1571d9021e7acd1cf
edomingues <edi.domingues@hurb.com>
2023-06-20 14:03:12 -0300

    :art: created use case to users

---
+++ domain/src/main/java/com/example/domain/users/usecase/GetUsersUseCase.kt
@@ -0,0 +1,25 @@
+package com.example.domain.users.usecase
+
+import com.example.domain.IUsersRepository
+import com.example.domain.base.Event
+import com.example.domain.base.ThrowableBase
+import com.example.domain.users.model.Users
+import io.reactivex.Observable
+
+class GetUsersUseCase(
+    private val repository: IUsersRepository
+) {
+    fun execute(): Observable<Event<Users>> {
+        return Observable.concat(Observable.just(Event.loading()), getWeatherForecast())
+    }
+
+    private fun getWeatherForecast(): Observable<Event<Users>>? {
+        return repository.getUsers()
+            .map { Event.data(it) }
+            .onErrorReturn { throwable ->
+                (throwable as? ThrowableBase)?.let {
+                    Event.error(it.type, it)
+                } ?: Event.error(throwable)
+            }.toObservable()
+    }
+}
