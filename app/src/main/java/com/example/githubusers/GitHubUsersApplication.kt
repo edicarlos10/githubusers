a4c987e058f3b7031c695086915ea3ce41c6a52b
edomingues <edi.domingues@hurb.com>
2023-06-20 12:25:15 -0300

    :construction: created main activity and set some configs to manifest

---
+++ app/src/main/java/com/example/githubusers/GitHubUsersApplication.kt
@@ -0,0 +1,11 @@
+package com.example.githubusers
+
+import android.app.Application
+
+class GitHubUsersApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+//        initModules()
+    }
+}
---
+++ app/src/main/java/com/example/githubusers/activity/GitHubUsersActivity.kt
@@ -0,0 +1,12 @@
+package com.example.githubusers.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubusers.R
+
+class GitHubUsersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_hub_users)
+    }
+}
