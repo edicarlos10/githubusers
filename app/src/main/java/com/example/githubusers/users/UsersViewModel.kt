5dbb4f9099e1242042844f37d58f9192b210847f
edomingues <edi.domingues@hurb.com>
2023-06-21 14:17:08 -0300

    :art: created fun to get repo from user in view model

--- app/src/main/java/com/example/githubusers/users/UsersViewModel.kt
+++ app/src/main/java/com/example/githubusers/users/UsersViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.domain.base.Event
 import com.example.domain.users.model.UserDetail
+import com.example.domain.users.model.UserRepos
 import com.example.domain.users.model.Users
 import com.example.domain.users.usecase.GetUserDetailUseCase
+import com.example.domain.users.usecase.GetUserReposUseCase
 import com.example.domain.users.usecase.GetUsersUseCase
 import com.example.domain.util.ISchedulerProvider
 import com.example.githubusers.base.BaseViewModel
@@ -14,7 +16,8 @@ import io.reactivex.rxkotlin.addTo
 class UsersViewModel(
     scheduler: ISchedulerProvider,
     private val getUsersUseCase: GetUsersUseCase,
-    private val getUserDetailUseCase: GetUserDetailUseCase
+    private val getUserDetailUseCase: GetUserDetailUseCase,
+    private val getUserReposUseCase: GetUserReposUseCase
 ) : BaseViewModel(scheduler) {
 
     private val _error = MutableLiveData<Event.Error?>()
@@ -32,6 +35,10 @@ class UsersViewModel(
     private val _userDetails = MutableLiveData<UserDetail?>()
     val userDetail: LiveData<UserDetail?>
         get() = _userDetails
+
+    private val _userRepos = MutableLiveData<List<UserRepos>?>()
+    val userRepos: LiveData<List<UserRepos>?>
+        get() = _userRepos
 
     fun getUsers() {
         getUsersUseCase.execute()
@@ -62,6 +69,26 @@ class UsersViewModel(
                 when (it) {
                     is Event.Data<*> -> {
                         _userDetails.value = it.data as UserDetail
+                    }
+
+                    is Event.Error -> {
+                        _error.value = it
+                    }
+
+                    else -> Unit
+                }
+            }.addTo(disposables)
+    }
+
+    fun getUserRepos(username: String) {
+        getUserReposUseCase.execute(username)
+            .subscribeOn(scheduler.backgroundThread())
+            .observeOn(scheduler.mainThread())
+            .subscribe {
+                _loading.value = it.isLoading()
+                when (it) {
+                    is Event.Data<List<UserRepos>> -> {
+                        _userRepos.value = it.data
                     }
 
                     is Event.Error -> {
