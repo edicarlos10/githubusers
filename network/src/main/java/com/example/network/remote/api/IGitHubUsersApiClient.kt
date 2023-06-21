8c545ae2e9052f6be48d8209e2eb7864a4338762
edomingues <edi.domingues@hurb.com>
2023-06-21 13:57:39 -0300

    :art: changed fun name

--- network/src/main/java/com/example/network/remote/api/IGitHubUsersApiClient.kt
+++ network/src/main/java/com/example/network/remote/api/IGitHubUsersApiClient.kt
@@ -6,7 +6,6 @@ import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface IGitHubUsersApiClient {
     @GET("users")
@@ -14,9 +13,9 @@ interface IGitHubUsersApiClient {
     ): Single<Response<List<UsersResponse>>>
 
     @GET("users/{username}")
-    fun getUser(username: String): Single<Response<UserDetailResponse>>
+    fun getUserDetail(username: String): Single<Response<UserDetailResponse>>
 
     @GET("users/{username}/repos")
-    fun getUserRepos(@Path("username") username: String,): Single<Response<List<UserDetailResponse>>>
+    fun getUserRepos(@Path("username") username: String): Single<Response<List<UserDetailResponse>>>
 
 }
