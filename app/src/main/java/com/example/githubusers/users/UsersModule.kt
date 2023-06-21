782afbf8bad0c60fcdf50c9ba7cc222f7944bc86
edomingues <edi.domingues@hurb.com>
2023-06-21 16:12:29 -0300

    :art: fix problems in usersModule

--- app/src/main/java/com/example/githubusers/users/UsersModule.kt
+++ app/src/main/java/com/example/githubusers/users/UsersModule.kt
@@ -1,6 +1,8 @@
 package com.example.githubusers.users
 
 import com.example.domain.IUsersRepository
+import com.example.domain.users.usecase.GetUserDetailUseCase
+import com.example.domain.users.usecase.GetUserReposUseCase
 import com.example.domain.users.usecase.GetUsersUseCase
 import com.example.network.UsersRepository
 import com.example.network.remote.IUsersRemoteData
@@ -9,8 +11,10 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val usersModule = module {
-    viewModel { UsersViewModel(scheduler = get(), getUsersUseCase = get()) }
+    viewModel { UsersViewModel(scheduler = get(), getUsersUseCase = get(), getUserDetailUseCase = get(), getUserReposUseCase = get()) }
     factory { GetUsersUseCase(repository = get()) }
+    factory { GetUserDetailUseCase(repository = get()) }
+    factory { GetUserReposUseCase(repository = get()) }
     single<IUsersRepository> { UsersRepository(usersRemoteData = get()) }
     factory<IUsersRemoteData> { UsersRemoteData(apiGitHubUsers = get()) }
 }
