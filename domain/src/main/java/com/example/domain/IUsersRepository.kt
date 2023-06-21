c0c83eacaceca77477a0a17ccace6ae6a45e881b
edomingues <edi.domingues@hurb.com>
2023-06-21 13:58:26 -0300

    :art: added new fun to interfaces

--- domain/src/main/java/com/example/domain/IUsersRepository.kt
+++ domain/src/main/java/com/example/domain/IUsersRepository.kt
@@ -1,8 +1,11 @@
 package com.example.domain
 
+import com.example.domain.users.model.UserDetail
 import com.example.domain.users.model.Users
 import io.reactivex.Single
 
 interface IUsersRepository {
     fun getUsers(): Single<List<Users>>
+
+    fun getUserDetail(username: String): Single<UserDetail>
 }
--- network/src/main/java/com/example/network/remote/IUsersRemoteData.kt
+++ network/src/main/java/com/example/network/remote/IUsersRemoteData.kt
@@ -1,8 +1,11 @@
 package com.example.network.remote
 
+import com.example.domain.users.model.UserDetail
 import com.example.domain.users.model.Users
 import io.reactivex.Single
 
 interface IUsersRemoteData {
     fun getUsers(): Single<List<Users>>
+
+    fun getUserDetail(username: String): Single<UserDetail>
 }
