43c0aeefd75c2d9906069b830b014651d1ae1a83
edomingues <edi.domingues@hurb.com>
2023-06-21 08:57:15 -0300

    :art: created fun to get repos

--- network/src/main/java/com/example/network/remote/api/IGitHubUsersApiClient.kt
+++ network/src/main/java/com/example/network/remote/api/IGitHubUsersApiClient.kt
@@ -5,12 +5,18 @@ import com.example.network.remote.response.UsersResponse
 import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface IGitHubUsersApiClient {
     @GET("users")
     fun getGitHubUsers(
     ): Single<Response<List<UsersResponse>>>
 
-    @GET("users/{login}")
-    fun getUser(sku: String): Single<Response<UserDetailResponse>>
+    @GET("users/{username}")
+    fun getUser(username: String): Single<Response<UserDetailResponse>>
+
+    @GET("users/{username}/repos")
+    fun getUserRepos(@Path("username") username: String,): Single<Response<List<UserDetailResponse>>>
+
 }
