89b8a4a4b5fba971bbefe1e6f0b20dbbb25976d5
edomingues <edi.domingues@hurb.com>
2023-06-21 14:09:34 -0300

    :art: created new fun to repos in interface

--- domain/src/main/java/com/example/domain/IUsersRepository.kt
+++ domain/src/main/java/com/example/domain/IUsersRepository.kt
@@ -1,6 +1,7 @@
 package com.example.domain
 
 import com.example.domain.users.model.UserDetail
+import com.example.domain.users.model.UserRepos
 import com.example.domain.users.model.Users
 import io.reactivex.Single
 
@@ -8,4 +9,6 @@ interface IUsersRepository {
     fun getUsers(): Single<List<Users>>
 
     fun getUserDetail(username: String): Single<UserDetail>
+
+    fun getUserRepos(username: String): Single<List<UserRepos>>
 }
--- network/src/main/java/com/example/network/remote/IUsersRemoteData.kt
+++ network/src/main/java/com/example/network/remote/IUsersRemoteData.kt
@@ -1,6 +1,7 @@
 package com.example.network.remote
 
 import com.example.domain.users.model.UserDetail
+import com.example.domain.users.model.UserRepos
 import com.example.domain.users.model.Users
 import io.reactivex.Single
 
@@ -8,4 +9,6 @@ interface IUsersRemoteData {
     fun getUsers(): Single<List<Users>>
 
     fun getUserDetail(username: String): Single<UserDetail>
+
+    fun getUserRepos(username: String): Single<List<UserRepos>>
 }
